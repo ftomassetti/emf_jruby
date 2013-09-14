00456e0d7ca6be9676abd52fa2a83e4ab1c4abe7
ftomassetti <FTomassetti@FTOMASSETTI-PC>
2013-09-14 11:05:44 +0200

    Minor correction

--- lib/emf/rgen_ext.rb
+++ lib/emf/rgen_ext.rb
@@ -119,13 +119,13 @@ class RGen::MetamodelBuilder::MMBase
 		def only_child_of_type(type)
 			selected = children.select {|c| c and c.is_a?(type)}
 			raise "Exactly one child of type #{type} expected, #{selected.count} found on #{self}" unless selected.count==1
-			selected
+			selected[0]
 		end
 
 		def only_child_deep_of_type(type)
 			selected = children_deep.select {|c| c and c.is_a?(type)}
 			raise "Exactly one child of type #{type} expected, #{selected.count} found on #{self}" unless selected.count==1
-			selected
+			selected[0]
 		end
 
 	end
